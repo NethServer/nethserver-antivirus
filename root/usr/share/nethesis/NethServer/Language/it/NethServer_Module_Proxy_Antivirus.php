facfa07141af3a76b9fc84176e419bde11ff8857
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-07-30 08:55:43 +0200

    web ui: move antivirus proxy configuration to nethserver-squidclamav package. Refs #1959

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Proxy_Antivirus.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-/* NethServer_Module_Proxy_Antivirus translation, language: it */
-
-$L['Antivirus_status'] = 'Antivirus';
-$L['Antivirus_Title'] = 'Antivirus';
--- root/usr/share/nethesis/NethServer/Template/Proxy/Antivirus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo $view->fieldset()->setAttribute('template', $T('Antivirus_status'))
-    ->insert($view->radioButton('status', 'enabled'))
-    ->insert($view->radioButton('status', 'disabled'));
-
-echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
