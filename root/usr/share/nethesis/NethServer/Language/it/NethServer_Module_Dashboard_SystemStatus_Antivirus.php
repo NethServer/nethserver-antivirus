9183ea72479ba001a7e02f456c3f844a681e501e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-09-25 10:51:53 +0200

    Remove Italian translation. Refs #3265

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_SystemStatus_Antivirus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$L['antivirus_title'] = 'Database antivirus';
-$L['status_label'] = 'Stato';
-$L['warning_label'] = 'Non aggiornato';
-$L['ok_label'] = 'Aggiornato';
-$L['timestamp_label'] = 'Ultimo aggiornamento';
