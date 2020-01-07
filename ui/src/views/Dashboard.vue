0fc8140f764d841e365d48ef7c09cfe53d06e359
Federico Ballarini <fed.ballarini@gmail.com>
2020-01-07 09:40:50 +0100

    ui. update dashboard title and app name (#10)
    Updated dashboard title and app name and changed h1 tag to h2 of pages title.

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{$t('dashboard.title')}}</h1>
+    <h2>{{$t('dashboard.title')}}</h2>
     <!-- error message -->
     <div v-if="errorMessage" class="alert alert-danger alert-dismissable">
       <button type="button" class="close" @click="closeErrorMessage()" aria-label="Close">
